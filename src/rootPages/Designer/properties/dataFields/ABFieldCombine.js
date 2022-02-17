f36df811877a3216b0a81d78997519ea650a6080
guyyoo <guy.konlawat@digiserve.org>
2022-02-17 11:17:35 +0700

    Add field Combine

--- src/rootPages/Designer/properties/dataFields/ABFieldCombine.js
+++ src/rootPages/Designer/properties/dataFields/ABFieldCombine.js
@@ -6,34 +6,108 @@
 import FFieldClass from "./ABField";
 
 export default function (AB) {
-    const uiConfig = AB.Config.uiSettings();
+   const uiConfig = AB.Config.uiSettings();
 
-    var ABField = FFieldClass(AB);
-    var L = ABField.L();
+   const ABField = FFieldClass(AB);
+   const L = ABField.L();
 
-    class ABFieldCombine extends ABField {
-        constructor() {
-            super("properties_abfield_combine", {
-            });
-        }
+   class ABFieldCombine extends ABField {
+      constructor() {
+         super("properties_abfield_combine", {
+            combinedFields: "",
+         });
+      }
 
-        ui() {
-            const FC = this.FieldClass();
-            const ids = this.ids;
-   
-            return super.ui([]);
-        }
+      ui() {
+         const ids = this.ids;
+         const FC = this.FieldClass();
 
-        /**
-         * @method FieldClass()
-         * Call our Parent's _FieldClass() helper with the proper key to return
-         * the ABFieldXXX class represented by this Property Editor.
-         * @return {ABFieldXXX Class}
-         */
-        FieldClass() {
-            return super._FieldClass("combined");
-        }
-    }
+         return super.ui([
+            {
+               id: ids.combinedFields,
+               name: "combinedFields",
+               view: "multicombo",
+               label: L("Combined Fields"),
+               labelWidth: uiConfig.labelWidthXLarge,
+               disallowEdit: true,
+               options: [],
+            },
+            {
+               view: "richselect",
+               name: "delimiter",
+               label: L("Delimiter"),
+               value: FC.defaultValues().delimiter,
+               labelWidth: uiConfig.labelWidthXLarge,
+               disallowEdit: true,
+               options: [
+                  { id: "plus", value: L("Plus ( + )") },
+                  { id: "dash", value: L("Dash ( - )") },
+                  { id: "period", value: L("Period ( . )") },
+                  { id: "space", value: L("Space ( )") },
+               ],
+            },
+         ]);
+      }
 
-    return ABFieldCombine;
+      /**
+       * @method FieldClass()
+       * Call our Parent's _FieldClass() helper with the proper key to return
+       * the ABFieldXXX class represented by this Property Editor.
+       * @return {ABFieldXXX Class}
+       */
+      FieldClass() {
+         return super._FieldClass("combined");
+      }
+
+      isValid() {
+         const isValid = super.isValid();
+
+         // validator.addError('columnName', L('ab.validation.object.name.unique', 'Field columnName must be unique (#name# already used in this Application)').replace('#name#', this.name) );
+
+         return isValid;
+      }
+
+      show() {
+         const ids = this.ids;
+
+         let fields = this.CurrentObject.fields((f) => {
+            return (
+               f.key == "string" ||
+               f.key == "LongText" ||
+               f.key == "number" ||
+               f.key == "date" ||
+               f.key == "datetime" ||
+               f.key == "boolean" ||
+               f.key == "list" ||
+               f.key == "email" ||
+               f.key == "user" ||
+               f.key == "AutoIndex" ||
+               f.key == "combined" ||
+               (f.isConnection &&
+                  // 1:M
+                  ((f.settings.linkType == "one" &&
+                     f.settings.linkViaType == "many") ||
+                     // 1:1 isSource = true
+                     (f.settings.linkType == "one" &&
+                        f.settings.linkViaType == "one" &&
+                        f.settings.isSource)))
+            );
+         }).map((f) => {
+            return {
+               id: f.id,
+               value: f.label,
+            };
+         });
+
+         super.show();
+
+         let $combinedFields = $$(ids.combinedFields);
+         if ($combinedFields) {
+            $combinedFields.define("options", fields);
+            $combinedFields.refresh();
+         }
+      }
+   }
+
+   return ABFieldCombine;
 }
