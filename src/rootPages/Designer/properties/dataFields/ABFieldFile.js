6502ee3c7d44e374737405fba0bc53e286b84ef9
guyyoo <guy.konlawat@digiserve.org>
2022-02-17 16:28:18 +0700

    Add field File

--- src/rootPages/Designer/properties/dataFields/ABFieldFile.js
+++ src/rootPages/Designer/properties/dataFields/ABFieldFile.js
@@ -6,34 +6,85 @@
 import FFieldClass from "./ABField";
 
 export default function (AB) {
-    const uiConfig = AB.Config.uiSettings();
+   var ABField = FFieldClass(AB);
+   var L = ABField.L();
 
-    var ABField = FFieldClass(AB);
-    var L = ABField.L();
+   class ABFieldFile extends ABField {
+      constructor() {
+         super("properties_abfield_file", {
+            fileSize: "",
+            fileType: "",
+         });
+      }
 
-    class ABFieldFile extends ABField {
-        constructor() {
-            super("properties_abfield_file", {
-            });
-        }
+      ui() {
+         const ids = this.ids;
 
-        ui() {
-            const FC = this.FieldClass();
-            const ids = this.ids;
-   
-            return super.ui([]);
-        }
+         return super.ui([
+            {
+               cols: [
+                  {
+                     view: "checkbox",
+                     name: "limitFileSize",
+                     labelRight: L("Size (MB)"),
+                     width: 120,
+                     labelWidth: 0,
+                     value: 1,
+                     click: function () {
+                        if (this.getValue()) $$(ids.fileSize).enable();
+                        else $$(ids.fileSize).disable();
+                     },
+                  },
+                  {
+                     view: "counter",
+                     name: "fileSize",
+                     id: ids.fileSize,
+                  },
+               ],
+            },
+            {
+               cols: [
+                  {
+                     view: "checkbox",
+                     name: "limitFileType",
+                     labelRight: L("Type"),
+                     width: 120,
+                     labelWidth: 0,
+                     value: 1,
+                     click: function () {
+                        if (this.getValue()) $$(ids.fileType).enable();
+                        else $$(ids.fileType).disable();
+                     },
+                  },
+                  {
+                     view: "text",
+                     name: "fileType",
+                     placeholder: L("txt,rtf,doc,docx,..."),
+                     id: ids.fileType,
+                  },
+               ],
+            },
+         ]);
+      }
 
-        /**
-         * @method FieldClass()
-         * Call our Parent's _FieldClass() helper with the proper key to return
-         * the ABFieldXXX class represented by this Property Editor.
-         * @return {ABFieldXXX Class}
-         */
-        FieldClass() {
-            return super._FieldClass("file");
-        }
-    }
+      clear() {
+         const ids = this.ids;
+console.log("ssss")
+         super.clear();
+         $$(ids.fileSize).setValue(0);
+         $$(ids.fileType).setValue("");
+      }
 
-    return ABFieldFile;
+      /**
+       * @method FieldClass()
+       * Call our Parent's _FieldClass() helper with the proper key to return
+       * the ABFieldXXX class represented by this Property Editor.
+       * @return {ABFieldXXX Class}
+       */
+      FieldClass() {
+         return super._FieldClass("file");
+      }
+   }
+
+   return ABFieldFile;
 }
