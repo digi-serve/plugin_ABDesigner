0981742c701d988eada07f60904a575ebc6175e0
guyyoo <guy.konlawat@digiserve.org>
2022-02-17 11:16:34 +0700

    Add field Boolean

--- src/rootPages/Designer/properties/dataFields/ABFieldBoolean.js
+++ src/rootPages/Designer/properties/dataFields/ABFieldBoolean.js
@@ -6,61 +6,58 @@
 import FFieldClass from "./ABField";
 
 export default function (AB) {
-    const uiConfig = AB.Config.uiSettings();
+   const ABField = FFieldClass(AB);
+   const L = ABField.L();
 
-    var ABField = FFieldClass(AB);
-    var L = ABField.L();
+   class ABFieldBoolean extends ABField {
+      constructor() {
+         super("properties_abfield_boolean", {});
+      }
 
-    class ABFieldBoolean extends ABField {
-        constructor() {
-            super("properties_abfield_boolean", {
-            });
-        }
+      ui() {
+         return super.ui([
+            {
+               name: "default",
+               view: "checkbox",
+               label: L("Default"),
+               labelPosition: "left",
+               labelWidth: 70,
+               labelRight: L("Uncheck"),
+               css: "webix_table_checkbox",
+               on: {
+                  onChange: function (newVal) {
+                     let checkLabel = L("Check");
+                     let uncheckLabel = L("Uncheck");
 
-        ui() {
-            const FC = this.FieldClass();
-            const ids = this.ids;
-   
-            return super.ui([
-                {
-                    name: "default",
-                    view: "checkbox",
-                    label: L("Default"),
-                    labelPosition: "left",
-                    labelWidth: 70,
-                    labelRight: L("Uncheck"),
-                    css: "webix_table_checkbox",
-                    on: {
-                        onChange: function (newVal, oldVal) {
-                            let checkLabel = L("Check");
-                            let uncheckLabel = L("Uncheck");
-        
-                            this.define("labelRight", newVal ? checkLabel : uncheckLabel);
-                            this.refresh();
-                        },
-                    },
-                },
-            ]);
-        }
+                     this.define(
+                        "labelRight",
+                        newVal ? checkLabel : uncheckLabel
+                     );
+                     this.refresh();
+                  },
+               },
+            },
+         ]);
+      }
 
-        /**
-         * @method FieldClass()
-         * Call our Parent's _FieldClass() helper with the proper key to return
-         * the ABFieldXXX class represented by this Property Editor.
-         * @return {ABFieldXXX Class}
-         */
-        FieldClass() {
-            return super._FieldClass("boolean");
-        }
+      /**
+       * @method FieldClass()
+       * Call our Parent's _FieldClass() helper with the proper key to return
+       * the ABFieldXXX class represented by this Property Editor.
+       * @return {ABFieldXXX Class}
+       */
+      FieldClass() {
+         return super._FieldClass("boolean");
+      }
 
-        isValid() {
-            var validator = super.isValid();
-        
-            // validator.addError('columnName', L('ab.validation.object.name.unique', 'Field columnName must be unique (#name# already used in this Application)').replace('#name#', this.name) );
-        
-            return validator;
-        }
-    }
+      isValid() {
+         const isValid = super.isValid();
 
-    return ABFieldBoolean;
+         // validator.addError('columnName', L('ab.validation.object.name.unique', 'Field columnName must be unique (#name# already used in this Application)').replace('#name#', this.name) );
+
+         return isValid;
+      }
+   }
+
+   return ABFieldBoolean;
 }
