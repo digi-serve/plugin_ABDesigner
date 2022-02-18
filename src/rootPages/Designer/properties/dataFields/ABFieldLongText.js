6059ce93f32221d445194cb47240d9cdd8fee7ac
nate <7259@pm.me>
2022-02-18 15:36:21 +0700

    add long text field

--- src/rootPages/Designer/properties/dataFields/ABFieldLongText.js
+++ src/rootPages/Designer/properties/dataFields/ABFieldLongText.js
@@ -6,34 +6,94 @@
 import FFieldClass from "./ABField";
 
 export default function (AB) {
-    const uiConfig = AB.Config.uiSettings();
+   const uiConfig = AB.Config.uiSettings();
 
-    var ABField = FFieldClass(AB);
-    var L = ABField.L();
+   const ABField = FFieldClass(AB);
+   const L = ABField.L();
 
-    class ABFieldLongText extends ABField {
-        constructor() {
-            super("properties_abfield_longtext", {
-            });
-        }
+   class ABFieldLongText extends ABField {
+      constructor() {
+         super("properties_abfield_longtext", {
+            default: "",
+            defaultCheckbox: "",
+         });
+      }
 
-        ui() {
-            const FC = this.FieldClass();
-            const ids = this.ids;
-   
-            return super.ui([]);
-        }
+      ui() {
+         const ids = this.ids;
 
-        /**
-         * @method FieldClass()
-         * Call our Parent's _FieldClass() helper with the proper key to return
-         * the ABFieldXXX class represented by this Property Editor.
-         * @return {ABFieldXXX Class}
-         */
-        FieldClass() {
-            return super._FieldClass("LongText");
-        }
-    }
+         return super.ui([
+            {
+               view: "layout",
+               cols: [
+                  {
+                     view: "label",
+                     label: L("Default Value:") + " ",
+                     align: "right",
+                     width: 100,
+                  },
+                  {
+                     id: ids.defaultCheckbox,
+                     view: "checkbox",
+                     width: 30,
+                     value: 0,
+                     on: {
+                        onChange: (newv) => {
+                           this.checkboxDefaultValue(newv);
+                        },
+                     },
+                  },
+                  {
+                     id: ids.default,
+                     view: "text",
+                     name: "default",
+                     placeholder: L("Enter default value"),
+                     disabled: true,
+                     labelWidth: uiConfig.labelWidthXLarge,
+                  },
+               ],
+            },
+            {
+               view: "checkbox",
+               name: "supportMultilingual",
+               disallowEdit: true,
+               labelRight: L("Support multilingual"),
+               labelWidth: uiConfig.labelWidthCheckbox,
+               value: false,
+            },
+         ]);
+      }
 
-    return ABFieldLongText;
+      /**
+       * @method FieldClass()
+       * Call our Parent's _FieldClass() helper with the proper key to return
+       * the ABFieldXXX class represented by this Property Editor.
+       * @return {ABFieldXXX Class}
+       */
+      FieldClass() {
+         return super._FieldClass("LongText");
+      }
+
+      populate(field) {
+         super.populate(field);
+         const value = field.settings.default === "" ? 0 : 1;
+         $$(this.ids.defaultCheckbox).setValue(value);
+      }
+
+      show() {
+         super.show();
+         $$(this.ids.defaultCheckbox).setValue(0);
+      }
+
+      checkboxDefaultValue(state) {
+         if (state == 0) {
+            $$(this.ids.default).disable();
+            $$(this.ids.default).setValue("");
+         } else {
+            $$(this.ids.default).enable();
+         }
+      }
+   }
+
+   return ABFieldLongText;
 }
