335ed6f724607aa198988f7b4300d95a23cc938c
guyyoo <guy.konlawat@digiserve.org>
2022-02-18 16:17:17 +0700

    Add field JSON

--- src/rootPages/Designer/properties/dataFields/ABFieldJson.js
+++ src/rootPages/Designer/properties/dataFields/ABFieldJson.js
@@ -6,34 +6,27 @@
 import FFieldClass from "./ABField";
 
 export default function (AB) {
-    const uiConfig = AB.Config.uiSettings();
+   const ABField = FFieldClass(AB);
 
-    var ABField = FFieldClass(AB);
-    var L = ABField.L();
+   class ABFieldJson extends ABField {
+      constructor() {
+         super("properties_abfield_json", {});
+      }
 
-    class ABFieldJson extends ABField {
-        constructor() {
-            super("properties_abfield_json", {
-            });
-        }
+      ui() {
+         return super.ui([]);
+      }
 
-        ui() {
-            const FC = this.FieldClass();
-            const ids = this.ids;
-   
-            return super.ui([]);
-        }
+      /**
+       * @method FieldClass()
+       * Call our Parent's _FieldClass() helper with the proper key to return
+       * the ABFieldXXX class represented by this Property Editor.
+       * @return {ABFieldXXX Class}
+       */
+      FieldClass() {
+         return super._FieldClass("json");
+      }
+   }
 
-        /**
-         * @method FieldClass()
-         * Call our Parent's _FieldClass() helper with the proper key to return
-         * the ABFieldXXX class represented by this Property Editor.
-         * @return {ABFieldXXX Class}
-         */
-        FieldClass() {
-            return super._FieldClass("json");
-        }
-    }
-
-    return ABFieldJson;
+   return ABFieldJson;
 }
