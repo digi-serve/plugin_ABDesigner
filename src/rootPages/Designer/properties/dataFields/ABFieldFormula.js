45f1b62db3ccf3f4e7bc669ad8ead89823686f0b
guyyoo <guy.konlawat@digiserve.org>
2022-02-18 11:56:17 +0700

    Add field Formula

--- src/rootPages/Designer/properties/dataFields/ABFieldFormula.js
+++ src/rootPages/Designer/properties/dataFields/ABFieldFormula.js
@@ -6,34 +6,188 @@
 import FFieldClass from "./ABField";
 
 export default function (AB) {
-    const uiConfig = AB.Config.uiSettings();
+   const uiConfig = AB.Config.uiSettings();
 
-    var ABField = FFieldClass(AB);
-    var L = ABField.L();
+   var ABField = FFieldClass(AB);
+   var L = ABField.L();
 
-    class ABFieldFormula extends ABField {
-        constructor() {
-            super("properties_abfield_formula", {
+   class ABFieldFormula extends ABField {
+      constructor() {
+         super("properties_abfield_formula", {
+            field: "",
+            fieldList: "",
+         });
+      }
+
+      ui() {
+         const App = AB._App;
+         const FC = this.FieldClass();
+         const ids = this.ids;
+
+         this.rowFilter = FC.rowFilter(App, ids.component, AB);
+
+         return super.ui([
+            {
+               view: "richselect",
+               name: "type",
+               label: L("Type"),
+               labelWidth: uiConfig.labelWidthMedium,
+               value: "sum",
+               options: [
+                  { id: "sum", value: L("Sum") },
+                  { id: "max", value: L("Max") },
+                  { id: "min", value: L("Min") },
+                  {
+                     id: "average",
+                     value: L("Average"),
+                  },
+                  {
+                     id: "count",
+                     value: L("Count"),
+                  },
+               ],
+            },
+            {
+               id: ids.field,
+               view: "richselect",
+               name: "field",
+               label: L("Field"),
+               labelWidth: uiConfig.labelWidthMedium,
+               options: {
+                  view: "suggest",
+                  body: {
+                     id: ids.fieldList,
+                     view: "list",
+                     template: this.itemTemplate,
+                     data: [],
+                  },
+               },
+               on: {
+                  onChange: () => {
+                     this.refreshFilter();
+                  },
+               },
+            },
+            this.rowFilter.ui,
+         ]);
+      }
+
+      getFieldList() {
+         const options = [];
+
+         const connectFields = this.CurrentObject.connectFields();
+         connectFields.forEach((f) => {
+            const objLink = f.datasourceLink;
+
+            objLink.fields().forEach((fLink) => {
+               // pull 'number' and 'calculate' fields from link objects
+               // if (fLink.key == "number" || fLink.key == "calculate") {
+               // NOTE: calculate fields does not support in queries
+               if (fLink.key == "number") {
+                  // NOTE: calculate fields does not support in queries
+                  options.push({
+                     // UUID:UUID
+                     id: `${f.id}:${fLink.id}`,
+                     field: f,
+                     fieldLink: fLink,
+                  });
+               }
             });
-        }
+         });
 
-        ui() {
-            const FC = this.FieldClass();
-            const ids = this.ids;
-   
-            return super.ui([]);
-        }
+         return options;
+      }
 
-        /**
-         * @method FieldClass()
-         * Call our Parent's _FieldClass() helper with the proper key to return
-         * the ABFieldXXX class represented by this Property Editor.
-         * @return {ABFieldXXX Class}
-         */
-        FieldClass() {
-            return super._FieldClass("formula");
-        }
-    }
+      itemTemplate(opt) {
+         return `[${opt.field.label}] ${opt.fieldLink.object.label} -> <i class="fa fa-${opt.fieldLink.icon}" aria-hidden="true"></i><b>${opt.fieldLink.label}</b>`;
+      }
 
-    return ABFieldFormula;
+      getSelectedField() {
+         const ids = this.ids;
+
+         const selectedId = $$(ids.field).getValue(); // fieldId:fieldLinkId
+         const selectedField = $$(ids.field)
+            .getList()
+            .data.find({ id: selectedId })[0];
+
+         return selectedField;
+      }
+
+      refreshFilter() {
+         const selectedField = this.getSelectedField();
+         if (
+            selectedField &&
+            selectedField.fieldLink &&
+            selectedField.fieldLink.object
+         ) {
+            // ABFieldFormulaComponent.rowFilter.applicationLoad(
+            //    selectedField.fieldLink.object.application
+            // );
+            this.rowFilter.fieldsLoad(selectedField.fieldLink.object.fields());
+            // ABFieldFormulaComponent.rowFilter.setValue({});
+         } else {
+            // ABFieldFormulaComponent.rowFilter.applicationLoad(null);
+            this.rowFilter.fieldsLoad([]);
+            // ABFieldFormulaComponent.rowFilter.setValue({});
+         }
+      }
+      /**
+       * @method FieldClass()
+       * Call our Parent's _FieldClass() helper with the proper key to return
+       * the ABFieldXXX class represented by this Property Editor.
+       * @return {ABFieldXXX Class}
+       */
+      FieldClass() {
+         return super._FieldClass("formula");
+      }
+
+      populate(field) {
+         const ids = this.ids;
+
+         super.populate(field);
+
+         if (field.settings.field) {
+            const selectedId = `${field.settings.field}:${field.settings.fieldLink}`;
+            $$(ids.field).setValue(selectedId || "");
+         } else {
+            $$(ids.field).setValue("");
+         }
+
+         this.refreshFilter();
+         this.rowFilter.setValue(field.settings.where || {});
+      }
+
+      show() {
+         const ids = this.ids;
+         const list = this.getFieldList();
+
+         $$(ids.fieldList).clearAll();
+         $$(ids.fieldList).parse(list);
+         super.show();
+      }
+
+      values() {
+         const ids = this.ids;
+         const settings = $$(ids.component).getValues();
+         const selectedField = this.getSelectedField();
+
+         if (selectedField) {
+            settings.field = selectedField.field.id;
+            settings.fieldLink = selectedField.fieldLink.id;
+            settings.object = selectedField.fieldLink.object.id;
+         } else {
+            settings.field = "";
+            settings.fieldLink = "";
+            settings.object = "";
+         }
+
+         settings.where = this.rowFilter.getValue();
+
+         $$(ids.component).setValues(settings);
+
+         return super.values();
+      }
+   }
+
+   return ABFieldFormula;
 }
