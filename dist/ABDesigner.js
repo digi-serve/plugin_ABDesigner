5ca234cd73f3af037c66ec21813a893ebaadd8e9
guyyoo <guy.konlawat@digiserve.org>
2022-05-06 11:47:50 +0700

    Put "require("./process/ABProcessParticipant.js")" back

--- dist/ABDesigner.js
+++ dist/ABDesigner.js
@@ -69297,6 +69297,7 @@ __webpack_require__.r(__webpack_exports__);
    var Processes = [];
    [
       __webpack_require__(/*! ./process/ABProcessEnd.js */ "./src/rootPages/Designer/properties/process/ABProcessEnd.js"),
+      __webpack_require__(/*! ./process/ABProcessParticipant.js */ "./src/rootPages/Designer/properties/process/ABProcessParticipant.js"),
       __webpack_require__(/*! ./process/ABProcessTaskEmail.js */ "./src/rootPages/Designer/properties/process/ABProcessTaskEmail.js"),
       __webpack_require__(/*! ./process/ABProcessTriggerLifecycle.js */ "./src/rootPages/Designer/properties/process/ABProcessTriggerLifecycle.js"),
       __webpack_require__(/*! ./process/ABProcessTaskService.js */ "./src/rootPages/Designer/properties/process/ABProcessTaskService.js"),
@@ -75836,6 +75837,173 @@ __webpack_require__.r(__webpack_exports__);
    }
 
    return UIProcessEnd;
+}
+
+
+/***/ }),
+
+/***/ "./src/rootPages/Designer/properties/process/ABProcessParticipant.js":
+/*!***************************************************************************!*\
+  !*** ./src/rootPages/Designer/properties/process/ABProcessParticipant.js ***!
+  \***************************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* export default binding */ __WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _ui_class__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../ui_class */ "./src/rootPages/Designer/ui_class.js");
+/* harmony import */ var _ABProcessParticipant_selectManagersUI__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./ABProcessParticipant_selectManagersUI */ "./src/rootPages/Designer/properties/process/ABProcessParticipant_selectManagersUI.js");
+/*
+ * ABProcessparticipant
+ *
+ * Display the form for entering the properties for a Process
+ * Participant Element
+ *
+ * @return {ClassUI} The Class Definition for this UI widget.
+ */
+
+
+
+/* harmony default export */ function __WEBPACK_DEFAULT_EXPORT__(AB) {
+   const UIClass = (0,_ui_class__WEBPACK_IMPORTED_MODULE_0__["default"])(AB);
+   var L = UIClass.L();
+
+   const ABProcessParticipantUsers = (0,_ABProcessParticipant_selectManagersUI__WEBPACK_IMPORTED_MODULE_1__["default"])(AB);
+
+   class ABProcessparticipant extends UIClass {
+      constructor() {
+         super("properties_process_participant", {
+            name: "",
+            users: "",
+         });
+
+         this.users = new ABProcessParticipantUsers(
+            this.ids.component + "_users_"
+         );
+      }
+
+      static get key() {
+         return "process.participant";
+      }
+      // {string}
+      // This should match the ABProcessParticipant.defaults().key value.
+
+      uiUser(obj) {
+         var usersUI = this.users.ui(obj ?? {});
+         return {
+            id: this.ids.users,
+            rows: [usersUI],
+            paddingY: 10,
+         };
+      }
+
+      ui(obj) {
+         // we are creating these on the fly, and should have CurrentApplication
+         // defined already.
+
+         let ids = this.ids;
+         let ui = {
+            id: ids.component,
+            rows: [
+               { view: "label", label: L("Process Participant:") },
+               {
+                  view: "label",
+                  label: L(
+                     "This element defines a group of users that are responsible for the tasks contained within."
+                  ),
+               },
+               {
+                  view: "form",
+                  id: ids.form,
+                  // width: 300,
+                  elements: [
+                     {
+                        id: ids.name,
+                        view: "text",
+                        label: L("Name"),
+                        name: "name",
+                        value: this.name,
+                     },
+                     // Select Users Template Goes here
+                  ],
+               },
+            ],
+         };
+         let usersUI = { id: ids.users };
+         // If we don't have any sub lanes, then offer the select user options:
+         if (obj?.laneIDs && obj?.laneIDs.length == 0) {
+            usersUI = this.uiUser(obj ?? {});
+         }
+         ui.rows[2].elements.push(usersUI);
+
+         return ui;
+      }
+
+      async init(AB) {
+         this.AB = AB;
+
+         return Promise.resolve();
+      }
+
+      // applicationLoad(application) {
+      //    super.applicationLoad(application);
+
+      //    $$(this.ids.objList).define("data", listObj);
+      //    $$(this.ids.objList).refresh();
+      // }
+
+      // show() {
+      //    super.show();
+      //    AppList.show();
+      // }
+
+      populate(obj) {
+         let ids = this.ids;
+
+         $$(ids.name).setValue(obj.name);
+
+         if (obj.laneIDs && obj.laneIDs.length == 0) {
+            var usersUI = this.uiUser(obj ?? {});
+            webix.ui(usersUI, $$(ids.users));
+         }
+      }
+
+      /**
+       * values()
+       * return an object hash representing the values for this component.
+       * @return {json}
+       */
+      // values() {
+      //    var obj = {};
+      //    var ids = this.ids;
+
+      //    obj.label = $$(ids.name)?.getValue();
+      //    obj.objectID = $$(ids.objList)?.getValue();
+      //    obj.lifecycleKey = $$(ids.lifecycleList).getValue();
+      //    obj.triggerKey = `${obj.objectID}.${obj.lifecycleKey}`;
+
+      //    return obj;
+      // }
+
+      values() {
+         var obj = {};
+         var ids = this.ids;
+
+         obj.label = $$(ids.name).getValue();
+
+         // if (obj.laneIDs.length == 0) {
+         var userDef = this.users.values();
+         Object.keys(userDef).forEach((k) => {
+            obj[k] = userDef[k];
+         });
+         // }
+         return obj;
+      }
+   }
+
+   return ABProcessparticipant;
 }
 
 
