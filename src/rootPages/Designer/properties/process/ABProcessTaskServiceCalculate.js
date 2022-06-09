496ea2df8b7bc6d807f876ee213b98a1bddba0ca
indy <indy@digiserve.org>
2022-06-09 16:28:01 +0700

    Add Calculate Task (Edited)
    https://github.com/digi-serve/plugin_ABDesigner/pull/37

--- src/rootPages/Designer/properties/process/ABProcessTaskServiceCalculate.js
+++ src/rootPages/Designer/properties/process/ABProcessTaskServiceCalculate.js
@@ -9,228 +9,222 @@
 import UI_Class from "../../ui_class";
 
 export default function (AB) {
-  const UIClass = UI_Class(AB);
-  var L = UIClass.L();
+   const UIClass = UI_Class(AB);
+   var L = UIClass.L();
 
-  class UIProcessServiceCalculate extends UIClass {
-    constructor() {
-      super("properties_process_service_calculate", {
-        name: "",
-        formulaText: "",
-        variableList: "",
+   class UIProcessServiceCalculate extends UIClass {
+      constructor() {
+         super("properties_process_service_calculate", {
+            name: "",
+            formulaText: "",
+            variableList: "",
 
-        variablePopup: "",
-        operatorPopup: "",
-      });
+            variablePopup: "",
+            operatorPopup: "",
+         });
 
-      this.element = null;
-    }
+         this.element = null;
+      }
 
-    static get key() {
-      return "Calculate";
-    }
+      static get key() {
+         return "Calculate";
+      }
 
-    ui() {
-      const ids = this.ids;
+      ui() {
+         const ids = this.ids;
 
-      webix.ui({
-        id: ids.variablePopup,
-        view: "popup",
-        hidden: true,
-        body: {
-          id: ids.variableList,
-          view: "list",
-          template: (item) => {
-            return item.value;
-          },
-          data: "",
-          on: {
-            onItemClick: function (id) {
-              var component = this.getItem(id);
+         webix.ui({
+            id: ids.variablePopup,
+            view: "popup",
+            hidden: true,
+            body: {
+               id: ids.variableList,
+               view: "list",
+               template: (item) => {
+                  return item.value;
+               },
+               data: "",
+               on: {
+                  onItemClick: function (id) {
+                     var component = this.getItem(id);
 
-              insertFormula(`{${component.value}}`);
+                     insertFormula(`{${component.value}}`);
 
-              $$(ids.variablePopup).hide();
+                     $$(ids.variablePopup).hide();
+                  },
+               },
             },
-          },
-        },
-      });
+         });
 
-      let insertFormula = (message) => {
-        let formula = $$(ids.formulaText).getValue();
+         const insertFormula = (message) => {
+            const formula = $$(ids.formulaText).getValue();
 
-        $$(ids.formulaText).setValue(`${formula}${message} `);
-      };
+            $$(ids.formulaText).setValue(`${formula}${message} `);
+         };
 
-      webix.ui({
-        id: ids.operatorPopup,
-        view: "popup",
-        hidden: true,
-        width: 180,
-        body: {
-          view: "list",
-          template: (item) => {
-            var template = "";
+         webix.ui({
+            id: ids.operatorPopup,
+            view: "popup",
+            hidden: true,
+            width: 180,
+            body: {
+               view: "list",
+               template: (item) => {
+                  var template = "";
 
-            if (item.icon) {
-              template += `<i class="fa fa-${item.icon}" aria-hidden="true"></i> `;
+                  if (item.icon) {
+                     template += `<i class="fa fa-${item.icon}" aria-hidden="true"></i> `;
+                  }
+
+                  if (item.label) {
+                     template += item.label;
+                  }
+
+                  return template;
+               },
+               data: [
+                  {
+                     label: L("+ Adds"),
+                     symbol: "+",
+                  },
+                  {
+                     label: L("- Subtracts"),
+                     symbol: "-",
+                  },
+                  {
+                     label: L("* Multiples"),
+                     symbol: "*",
+                  },
+                  {
+                     label: L("/ Divides"),
+                     symbol: "/",
+                  },
+                  {
+                     label: L("( Open Bracket"),
+                     symbol: "(",
+                  },
+                  {
+                     label: L(") Closed Bracket"),
+                     symbol: ")",
+                  },
+               ],
+               on: {
+                  onItemClick: function (id, e, node) {
+                     var component = this.getItem(id);
+
+                     insertFormula(component.symbol);
+
+                     $$(ids.operatorPopup).hide();
+                  },
+               },
+            },
+         });
+         let labelWidth = 120;
+         return {
+            rows: [
+               {
+                  //     id: id,
+                  view: "form",
+                  elementsConfig: {
+                     labelWidth: labelWidth,
+                  },
+                  elements: [
+                     {
+                        id: ids.name,
+                        view: "text",
+                        label: L("Name"),
+                        name: "name",
+                        value: this.name,
+                     },
+
+                     {
+                        id: ids.formulaText,
+                        view: "texthighlight",
+                        height: 200,
+                        label: L("Formula"),
+                        type: "textarea",
+                        value: this.formulaText || "",
+                        highlight: (text) => {
+                           return text;
+                        },
+                     },
+                     {
+                        cols: [
+                           {
+                              width: labelWidth,
+                              fillspace: true,
+                           },
+                           {
+                              view: "button",
+                              css: "webix_primary",
+                              type: "icon",
+                              icon: "fa fa-at",
+                              label: L("Parameters"),
+                              click: function () {
+                                 // show popup
+                                 $$(ids.variablePopup).show(this.$view);
+                              },
+                           },
+                           {
+                              view: "button",
+                              css: "webix_primary",
+                              type: "icon",
+                              icon: "fa fa-hashtag",
+                              label: L("Operators"),
+                              click: function () {
+                                 // show popup
+                                 $$(ids.operatorPopup).show(this.$view);
+                              },
+                           },
+                        ],
+                     },
+                  ],
+               },
+            ],
+         };
+      }
+
+      populate(element) {
+         const ids = this.ids;
+
+         const list = (element?.process?.processDataFields(element) || []).map(
+            (item) => {
+               return {
+                  id: item.key,
+                  value: item.label,
+               };
             }
+         );
 
-            if (item.label) {
-              template += item.label;
-            }
+         $$(ids.name).setValue(element.name);
+         $$(ids.formulaText).setValue(element.formulaText);
+         $$(ids.variableList).define("data", list);
+         $$(ids.variableList).refresh();
+      }
 
-            return template;
-          },
-          data: [
-            {
-              label: L("+ Adds"),
-              symbol: "+",
-            },
-            {
-              label: L("- Subtracts"),
-              symbol: "-",
-            },
-            {
-              label: L("* Multiples"),
-              symbol: "*",
-            },
-            {
-              label: L("/ Divides"),
-              symbol: "/",
-            },
-            {
-              label: L("( Open Bracket"),
-              symbol: "(",
-            },
-            {
-              label: L(") Closed Bracket"),
-              symbol: ")",
-            },
-          ],
-          on: {
-            onItemClick: function (id, e, node) {
-              var component = this.getItem(id);
+      values() {
+         const obj = {};
+         const ids = this.ids;
 
-              insertFormula(component.symbol);
+         obj.name = $$(ids.name)?.getValue();
+         obj.formulaText = $$(ids.formulaText).getValue();
 
-              $$(ids.operatorPopup).hide();
-            },
-          },
-        },
-      });
-      let labelWidth = 120;
-      return {
-        rows: [
-          {
-            //     id: id,
-            view: "form",
-            elementsConfig: {
-              labelWidth: labelWidth,
-            },
-            elements: [
-              {
-                id: ids.name,
-                view: "text",
-                label: L("*Name"),
-                name: "name",
-                value: this.name,
-              },
+         return obj;
+      }
 
-              {
-                id: ids.formulaText,
-                view: "texthighlight",
-                height: 200,
-                label: L("*Formula"),
-                type: "textarea",
-                value: this.formulaText || "",
-                highlight: (text) => {
-                  // list.forEach(function (item) {
-                  //   text = text.replace(
-                  //     new RegExp(`{${item.value}}`, "g"),
-                  //     `<span style='background: #90adb5; color:#000000;'>{${item.value}}</span>`
-                  //   );
-                  // });
-                  return text;
-                },
-              },
-              {
-                cols: [
-                  {
-                    width: labelWidth,
-                    fillspace: true,
-                  },
-                  {
-                    view: "button",
-                    css: "webix_primary",
-                    type: "icon",
-                    icon: "fa fa-at",
-                    label: L("*Parameters"),
-                    click: function () {
-                      // show popup
-                      $$(ids.variablePopup).show(this.$view);
-                    },
-                  },
-                  {
-                    view: "button",
-                    css: "webix_primary",
-                    type: "icon",
-                    icon: "fa fa-hashtag",
-                    label: L("*Operators"),
-                    click: function () {
-                      // show popup
-                      $$(ids.operatorPopup).show(this.$view);
-                    },
-                  },
-                ], //End Column
-              },
-            ], //End element
-          },
-        ], //End row
-      }; //End Return
-    } //End UI
+      /**
+       * @method propertiesStash()
+       * pull our values from our property panel.
+       * @param {string} id
+       *        the webix $$(id) of the properties panel area.
+       */
+      propertiesStash(id) {
+         const ids = this.propertyIDs(id);
 
-    populate(element) {
-      const ids = this.ids;
+         this.name = this.property(ids.name);
+         this.formulaText = this.property(ids.formulaText);
+      }
+   } //End
 
-      let list = (element?.process?.processDataFields(element) || []).map(
-        (item) => {
-          return {
-            id: item.key,
-            value: item.label,
-          };
-        }
-      );
-
-      $$(ids.name).setValue(element.name);
-      $$(ids.formulaText).setValue(element.formulaText);
-      $$(ids.variableList).define("data", list);
-      $$(ids.variableList).refresh();
-    }
-
-    values() {
-      var obj = {};
-      var ids = this.ids;
-
-      obj.label = $$(ids.name)?.getValue();
-      obj.formulaText = $$(ids.formulaText).getValue();
-
-      return obj;
-    }
-
-    /**
-     * @method propertiesStash()
-     * pull our values from our property panel.
-     * @param {string} id
-     *        the webix $$(id) of the properties panel area.
-     */
-    propertiesStash(id) {
-      let ids = this.propertyIDs(id);
-
-      this.name = this.property(ids.name);
-      this.formulaText = this.property(ids.formulaText);
-    }
-  } //End
-
-  return UIProcessServiceCalculate;
+   return UIProcessServiceCalculate;
 }
