51f1bdcb0df8796d7912fb034e0119b764928955
wongpraatan <wongpratan@hotmail.com>
2022-09-06 18:53:21 +0700

    +Migrate conditional container widget

--- src/rootPages/Designer/editors/EditorManager.js
+++ src/rootPages/Designer/editors/EditorManager.js
@@ -10,6 +10,7 @@ export default function (AB) {
    // All the ABField Component Inerfaces available.
    [
       require("./views/ABViewCarousel"),
+      require("./views/ABViewConditionalContainer"),
       require("./views/ABViewContainer"),
       require("./views/ABViewDataview"),
       require("./views/ABViewDetail"),
