66e39a57b91c0679b2a8ab102d61c405dad71011
wongpraatan <wongpratan@hotmail.com>
2022-11-18 15:00:51 +0700

    *Fix populate elements of the form widget - https://github.com/digi-serve/plugin_ABDesigner/issues/92

--- src/rootPages/Designer/properties/views/ABViewForm.js
+++ src/rootPages/Designer/properties/views/ABViewForm.js
@@ -323,7 +323,7 @@ export default function (AB) {
 
          // update properties when a field component is deleted
          view.views().forEach((v) => {
-            if (v instanceof this.AB.Class.ABViewFormComponent)
+            if (v instanceof this.AB.Class.ABViewFormItem)
                v.once("destroyed", () => this.populate(view));
          });
 
@@ -589,10 +589,12 @@ export default function (AB) {
 
       refreshDefaultButton() {
          const ids = this.ids;
+         const ABViewFormButton =
+            this.AB.Class.ABViewManager.viewClass("button");
 
          // If default button is not exists, then skip this
          let defaultButton = this.views(
-            (v) => v instanceof this.ABViewFormButton && v.settings.isDefault
+            (v) => v instanceof ABViewFormButton && v.settings.isDefault
          )[0];
 
          // Add a default button
