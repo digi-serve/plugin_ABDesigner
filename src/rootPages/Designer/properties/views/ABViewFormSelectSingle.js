2d9233b9eb47af6705ccae616f24123c8a86420a
wongpraatan <wongpratan@hotmail.com>
2023-01-06 16:07:40 +0700

    *Migrate properties of elements of the form widget

---
+++ src/rootPages/Designer/properties/views/ABViewFormSelectSingle.js
@@ -0,0 +1,110 @@
+/*
+ * ABViewFormSelectSingle
+ * A Property manager for our ABViewFormSelectSingle definitions
+ */
+
+import FABView from "./ABView";
+
+export default function (AB) {
+   const BASE_ID = "properties_abview_form_select_single";
+
+   const ABView = FABView(AB);
+   const L = ABView.L();
+
+   class ABViewFormSelectSingleProperty extends ABView {
+      constructor() {
+         super(BASE_ID, {
+            // Put our ids here
+            type: "",
+         });
+
+         this.AB = AB;
+      }
+
+      static get key() {
+         return "selectsingle";
+      }
+
+      ui() {
+         const ids = this.ids;
+
+         return super.ui([
+            {
+               id: ids.type,
+               name: "type",
+               view: "richselect",
+               label: L("Type"),
+               options: [
+                  {
+                     id: "richselect",
+                     value: L("Select list"),
+                  },
+                  {
+                     id: "radio",
+                     value: L("Radio"),
+                  },
+               ],
+            },
+         ]);
+      }
+
+      async init(AB) {
+         this.AB = AB;
+
+         await super.init(AB);
+      }
+
+      populate(view) {
+         super.populate(view);
+
+         const ids = this.ids;
+         const ABViewFormSelectSinglePropertyComponentDefaults =
+            this.defaultValues();
+
+         $$(ids.type).setValue(
+            view.settings.type ||
+               ABViewFormSelectSinglePropertyComponentDefaults.type
+         );
+      }
+
+      defaultValues() {
+         const ViewClass = this.ViewClass();
+
+         let values = null;
+
+         if (ViewClass) {
+            values = ViewClass.defaultValues();
+         }
+
+         return values;
+      }
+
+      /**
+       * @method values
+       * return the values for this form.
+       * @return {obj}
+       */
+      values() {
+         const ids = this.ids;
+
+         const $component = $$(ids.component);
+
+         const values = super.values() ?? {};
+         values.settings = $component.getValues() ?? {};
+         values.settings.type = $$(ids.type).getValue();
+
+         return values;
+      }
+
+      /**
+       * @method FieldClass()
+       * A method to return the proper ABViewXXX Definition.
+       * NOTE: Must be overwritten by the Child Class
+       */
+      ViewClass() {
+         return super._ViewClass("selectsingle");
+      }
+   }
+
+   return ABViewFormSelectSingleProperty;
+}
