1e18583168def08b4ca5ce5eb704c9442ffa6f57
wongpraatan <wongpratan@hotmail.com>
2023-04-19 14:27:07 +0700

    *Fix pivot widget - https://github.com/digi-serve/plugin_ABDesigner/issues/179

--- src/rootPages/Designer/editors/EditorManager.js
+++ src/rootPages/Designer/editors/EditorManager.js
@@ -31,6 +31,7 @@ export default function (AB) {
       require("./views/ABViewLayout"),
       require("./views/ABViewMenu"),
       require("./views/ABViewPage"),
+      require("./views/ABViewPivot"),
       require("./views/ABViewTab"),
       require("./views/ABViewText"),
    ].forEach((E) => {
