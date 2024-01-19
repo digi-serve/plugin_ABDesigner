b106aee7bb312a3084f87a1d93d9c7a243c3c192
Johnny Hausman <johnny@hausmanfamily.com>
2024-01-18 18:08:43 -0600

    [wip] refactor Plugin to install itself into an installation array.

--- index.js
+++ index.js
@@ -4,28 +4,28 @@ import Labels from "./src/labels/labels.js";
 
 import designerCSS from "./styles/Designer.css";
 
-if (window.__ABBS) {
-   window.__ABBS.addPlugin({
-      version: "0.0.0",
-      key: "ABDesigner",
-      apply: function (AB) {
-         // At this point, the Plugin should already have loaded all it's definitions
-         // into the AB Factory
-         AB.pluginLoad(ApplicationFactory(AB));
+window.__AB_Plugins = window.__AB_Plugins || [];
 
-         // var labels = Labels.en; /* default */;
-         // var lang =AB.Multilingual.currentLanguage();
+window.__AB_Plugins.push({
+   version: "0.0.0",
+   key: "ABDesigner",
+   apply: function (AB) {
+      // At this point, the Plugin should already have loaded all it's definitions
+      // into the AB Factory
+      AB.pluginLoad(ApplicationFactory(AB));
 
-         // if (Labels[lang]) {
-         //    labels = Labels[lang];
-         // }
-         // AB.pluginLabelLoad("ABDesigner", labels);
-      },
-      definitions: function () {
-         return definitions;
-      },
-      labels: function (lang) {
-         return Labels[lang] || Labels.en;
-      },
-   });
-}
+      // var labels = Labels.en; /* default */;
+      // var lang =AB.Multilingual.currentLanguage();
+
+      // if (Labels[lang]) {
+      //    labels = Labels[lang];
+      // }
+      // AB.pluginLabelLoad("ABDesigner", labels);
+   },
+   definitions: function () {
+      return definitions;
+   },
+   labels: function (lang) {
+      return Labels[lang] || Labels.en;
+   },
+});
