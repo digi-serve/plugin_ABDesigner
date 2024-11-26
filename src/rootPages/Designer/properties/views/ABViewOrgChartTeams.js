cad5a6140f3abdcbdf1c71273dbc79ea655a1061
nh758 <7259@pm.me>
2024-11-26 16:38:08 +0700

    add entity dc setting

--- src/rootPages/Designer/properties/views/ABViewOrgChartTeams.js
+++ src/rootPages/Designer/properties/views/ABViewOrgChartTeams.js
@@ -28,7 +28,6 @@ export default function (AB) {
             depth: "",
             draggable: "",
             dropContentToCreate: "",
-            color: "",
             pan: "",
             zoom: "",
             height: "",
@@ -48,6 +47,7 @@ export default function (AB) {
             dataPanelDCsAdd: "",
             strategyColorPopup: "",
             strategyColorForm: "",
+            entityDatacollection: "",
          });
          this.AB = AB;
          const contentFieldFilter = (this.contentFieldFilter =
@@ -596,6 +596,17 @@ export default function (AB) {
                on: { onChange: () => this.onChange() },
             },
             {
+               id: ids.entityDatacollection,
+               name: "entityDatacollection",
+               view: "richselect",
+               label: L("Entity"),
+               labelWidth: uiConfig.labelWidthLarge,
+               options: [],
+               on: {
+                  onChange: () => this.onChange(),
+               },
+            },
+            {
                id: ids.depth,
                name: "depth",
                hidden: true, // NOTE: use choose Connect Fields option
@@ -603,14 +614,6 @@ export default function (AB) {
                label: L("Depth"),
                labelWidth: uiConfig.labelWidthLarge,
                value: 0,
-               on: { onChange: () => this.onChange() },
-            },
-            {
-               id: ids.color,
-               name: "color",
-               view: "colorpicker",
-               label: L("Color"),
-               labelWidth: uiConfig.labelWidthLarge,
                on: { onChange: () => this.onChange() },
             },
             {
@@ -773,6 +776,8 @@ export default function (AB) {
          $dataCollection.define("options", dcOptions);
          $dataCollection.define("value", datacollectionId);
          $dataCollection.refresh();
+         $$(this.ids.entityDatacollection).define("options", dcOptions);
+         $$(this.ids.entityDatacollection).refresh();
       }
 
       refreshValueFieldOptions(fieldValues = []) {
